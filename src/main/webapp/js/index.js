860b8cc6b824ec3e41d249bcfe13f4076ab18ab8
Arun <arun.s.shinde@oracle.com>
2025-03-21 01:03:20 +0530

    Test the Jenkins deployment

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -4,9 +4,21 @@ async function fetchAndSetBackgroundImages() {
 
         const data = await response.json();
 
-        document.getElementById('slide1').style.backgroundImage = `url(${data.imageUrl1})`;
-        document.getElementById('slide2').style.backgroundImage = `url(${data.imageUrl2})`;
-        document.getElementById('slide3').style.backgroundImage = `url(${data.imageUrl3})`;
+        // Check if the response has the imageList array
+        if (data && data.imageList && Array.isArray(data.imageList)) {
+            const imageList = data.imageList;
+
+            // Check if there are enough images in the list
+            if (imageList.length >= 3) {
+                document.getElementById('slide1').style.backgroundImage = `url(${imageList[0].imageUrl})`;
+                document.getElementById('slide2').style.backgroundImage = `url(${imageList[1].imageUrl})`;
+                document.getElementById('slide3').style.backgroundImage = `url(${imageList[2].imageUrl})`;
+            } else {
+                console.error('Not enough images in the response.');
+            }
+        } else {
+            console.error('Invalid JSON response format.');
+        }
 
     } catch (error) {
         console.error('Error fetching images:', error);
