4610fb3189c8cb604f3b7d5cf82158eea6bbeaa3
Arun <arun.s.shinde@oracle.com>
2025-03-26 18:16:25 +0530

    Resultset close issue fix

--- src/main/java/utility/DatabaseUtility.java
+++ src/main/java/utility/DatabaseUtility.java
@@ -8,19 +8,17 @@ import java.sql.Statement;
 
 public class DatabaseUtility {
 
-    public final String THIS_COMPONENT_NAME = this.getClass().getName();
-
     public static ResultSet executeSelectQuery(String query) throws SQLException, IOException {
-        System.out.println("getDataFromDatabase");
+        System.out.println("::getDataFromDatabase");
 
         Connection conn = DatabaseConnection.getConnection();
         assert conn != null;
         Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM LANDINGPAGE");
 
-        stmt.close();
+        //rs.close();
+        //stmt.close();
         //conn.close(); // Don't close the connection here when using a connection pool
-        return rs;
+        return stmt.executeQuery(query);
     }
 
 }
