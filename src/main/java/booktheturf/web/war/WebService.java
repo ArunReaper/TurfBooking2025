0e8ff74e71c19ac4c0468cb8a362b21b65950f03
Arun <arun.s.shinde@oracle.com>
2025-03-27 17:17:23 +0530

    Login Service Addition.

--- src/main/java/booktheturf/web/war/WebService.java
+++ src/main/java/booktheturf/web/war/WebService.java
@@ -7,10 +7,7 @@ import java.util.Date;
 import java.sql.SQLException;
 import java.io.IOException;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -72,7 +69,7 @@ public class WebService {
 	 *
 	 * @return A web response.
 	 */
-	@GET
+	@POST
 	@Path("/login")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
